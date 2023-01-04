0ec86809a71e605e1488c17b001f70ab78555ade
Timothy Corry <tocrry@hotmail.com.au>
2023-01-04 17:04:13 +0700

    Initial Commit

---
+++ GitHubDemo1/GitHubDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemo1App.swift
+//  GitHubDemo1
+//
+//  Created by Corry Timothy on 4/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
