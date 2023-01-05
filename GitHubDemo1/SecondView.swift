0a3272b57d761ef58d5e256ab1c13e8c5d2e51ff
Timothy Corry <tocrry@hotmail.com.au>
2023-01-05 16:39:45 +0700

    create secondview

---
+++ GitHubDemo1/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubDemo1
+//
+//  Created by Corry Timothy on 5/1/2023.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
